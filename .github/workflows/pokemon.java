aa1511b31b43c32c27acdb6165ffd7bf5b90646c
relias1114 <71903711+relias1114@users.noreply.github.com>
2022-04-21 12:44:18 -0400

    Update pokemon.java

--- .github/workflows/pokemon.java
+++ .github/workflows/pokemon.java
@@ -1,36 +1,51 @@
-# This is a basic workflow to help you get started with Actions
+package doc.resources;
 
-name: CI
-
-# Controls when the workflow will run
-on:
-  # Triggers the workflow on push or pull request events but only for the main branch
-  push:
-    branches: [ main ]
-  pull_request:
-    branches: [ main ]
-
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
-
-# A workflow run is made up of one or more jobs that can run sequentially or in parallel
-jobs:
-  # This workflow contains a single job called "build"
-  build:
-    # The type of runner that the job will run on
-    runs-on: ubuntu-latest
-
-    # Steps represent a sequence of tasks that will be executed as part of the job
-    steps:
-      # Checks-out your repository under $GITHUB_WORKSPACE, so your job can access it
-      - uses: actions/checkout@v3
-
-      # Runs a single command using the runners shell
-      - name: Run a one-line script
-        run: echo Hello, world!
-
-      # Runs a set of commands using the runners shell
-      - name: Run a multi-line script
-        run: |
-          echo Add other actions to build,
-          echo test, and deploy your project.
+public class Pokemon {
+	private int health = 120;  //intialize in the extension or usage 
+	private int damage;
+	private String name;
+	private String type; //make an int that is used in an array 
+	public Pokemon (int health1, int damage1, String name1, String type1) {
+		health = health1;
+		damage = damage1;
+		name = name1;
+		type = type1;
+		
+	}
+	public int getHealth(){
+		return health;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public String getName () {
+		return name;
+	}
+	public String getType() {
+		return type;
+	}
+	public int hit(int userHealth, int attackDamage) {
+		if (userHealth ==0 || (userHealth-attackDamage)<0) {
+			return 0;
+		} else {
+			return userHealth-attackDamage;
+		}
+		
+	}
+	public boolean superEffective(String type1, String type2) {
+		boolean effective = false;
+		if (type1.equals(type2)) {
+			return effective;
+		}
+		if (type1.equals("fire")&& type2.equals("grass")) {
+			effective = true;
+		}
+		if(type1.equals("water")&& type2.equals("fire")) {
+			effective = true;
+		}
+		if (type1.equals("grass")&& type2.equals("water")) {
+			effective = true;
+		}
+		return effective;
+}
+	}
